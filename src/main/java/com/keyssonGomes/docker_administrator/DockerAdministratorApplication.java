5c321c28df2d9970d44395999ad2bdb03d15efe7
keyssong <keyssoncardoso0@gmail.com>
2024-11-10 20:40:14 -0300

    Construido services e controllers

---
+++ src/main/java/com/keyssonGomes/docker_administrator/DockerAdministratorApplication.java
@@ -0,0 +1,13 @@
+package com.keyssonGomes.docker_administrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerAdministratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerAdministratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keyssonGomes/docker_administrator/DockerAdministratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyssonGomes.docker_administrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerAdministratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
